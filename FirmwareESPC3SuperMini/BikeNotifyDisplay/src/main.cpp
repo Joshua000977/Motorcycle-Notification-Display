757d5ff98b190cc5111c737b7cb54ad8539e5f2a
jt <joshua.tidl@edu.htl-klu.at>
2026-05-09 19:21:57 +0200

    edit .gitignore

--- FirmwareESPC3SuperMini/BikeNotifyDisplay/src/main.cpp
+++ FirmwareESPC3SuperMini/BikeNotifyDisplay/src/main.cpp
@@ -2,7 +2,6 @@
 #include <NimBLEDevice.h>
 
 #define DEVICE_NAME "MotoNotifyDisplay"
-
 #define SERVICE_UUID "6e400001-b5a3-f393-e0a9-e50e24dcca9e"
 #define CHARACTERISTIC_UUID "6e400002-b5a3-f393-e0a9-e50e24dcca9e"
 
